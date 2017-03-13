e4ae98fdeff664079af42f5a0bf7af174a623b0e
Flavio dos Reis Martins <flaviodosreismartins@gmail.com>
2017-03-13 12:51:30 -0700

    trying auth

---
+++ assets/js/fbkey.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBG-lHAs_mDOMNDZS3IEVcb7WLUSQydvG4",
+  authDomain: "rpsgame-95e12.firebaseapp.com",
+  databaseURL: "https://rpsgame-95e12.firebaseio.com",
+  storageBucket: "rpsgame-95e12.appspot.com",
+  messagingSenderId: "538964743464"
+};
+firebase.initializeApp(config);
--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,125 @@ function rps(p1, p2){
 	}
 }
 
+// firebase 
+var db = firebase.database();
+
+$(document).ready(function(){
+	//assign faribase to variable
+	var db = firebase.database();
+
+	//variables 
+	var player;
+
+
+	// Click Events	=================
+	// Start Form
+	$("#player-submit").on("click", function(){
+		player = $("#player-name").val().trim();
+		console.log(player);
+		
+		write("p1",player,"none");
+	});
+	// P1 Panel click event
+	$(".p1").on("click", function(){
+		var chose = $(this).attr("data-value").trim();
+		console.log("p1",player,chose);
+			
+	});
+	// P2 Panel click event
+	$(".p2").on("click", function(){
+		var chose = $(this).attr("data-value").trim();
+		console.log(chose);	
+		
+	});
+
+});
+
+
+
+
+	var test;
+
+	//===============================
+	var connectedRef = db.ref(".info/connected");
+	var test = db.ref("players");
+	
+	// When the client's connection state changes...
+	connectedRef.on("value", function(snap) {
+		test = snap;
+	  // If they are connected..
+	  //if (snap.val()) {
+
+	    // Add user to the connections list.
+	    //var con = connectionsRef.push(true);
+
+	    // Remove user from the connection list when they disconnect.
+	    //con.onDisconnect().remove();
+	  //}
+	  //console.log(snap + "  ./info/connected after if() ");
+	});
+
+
+
+
+	//-------------------------- insert player in data base
+	function write(id, name, rps) {
+	  db.ref('players/' + id).set({
+	    id: id,
+	    username: name,
+	    chose: rps,
+	  });
+	}
+	//-------------------------- return what the opponent chosed
+	function read(player){
+		var result = "";
+		var opponent = db.ref('players/' + player + '/chose');
+		opponent.on('value', function(snapshot) {
+		  result = snapshot.val();
+		});
+		return result;
+	}
+	//--------------------------
+	function playerPosition(){
+
+		var connectedRef = db.ref(".info/connected");
+
+		// When the client's connection state changes...
+		connectedRef.on("value", function(snap) {
+			console.log(snap + "  ./info/connected");
+		  // If they are connected..
+		  if (snap.val()) {
+
+		    // Add user to the connections list.
+		    var con = connectionsRef.push(true);
+
+		    // Remove user from the connection list when they disconnect.
+		    con.onDisconnect().remove();
+		  }
+		  console.log(snap + "  ./info/connected after if() ");
+		});
+
+		// When first loaded or when the connections list changes...
+		connectionsRef.on("value", function(snap) {
+
+		  // Display the viewer count in the html.
+		  // The number of online users is the number of children in the connections list.
+		  //$("#watchers").html(snap.numChildren());
+		});
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
