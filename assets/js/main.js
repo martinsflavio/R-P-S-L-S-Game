d6c849a3e698ba25a1f8628e9522efd2d5ecb4f6
Flavio dos Reis Martins <flaviodosreismartins@gmail.com>
2017-03-16 01:07:04 -0700

    refactoring HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function rps(p1, p2){
+function rpsls(p2, p1){
 	p1 = p1.toLowerCase();
 	p2 = p2.toLowerCase();
 	
@@ -56,33 +56,90 @@ const $$ = {
 	btnStart : $('#btn-start'),
 }
 //Create references
-const db = {
+var db = {
 	root : firebase.database(),
-	players : firebase.database().ref('players'),
-	p1 : firebase.database().ref('players/1'),
-	p2 : firebase.database().ref('players/2'),
+	players : firebase.database().ref('/players'),
+	p1 : firebase.database().ref('/players/1'),
+	p2 : firebase.database().ref('/players/2'),
 	connected : firebase.database().ref('.info/connected'),
 }
 
 //============= Connecting =======================
+//Take the .push() key and store in local variables
+// all players key
 var players;
-var me;
+// this player index who point to players array
+var me; 
 db.root.ref('.info/connected').on('value', snap => {
-	console.log(snap.val());
-  
+	
   if (snap.val()) {
-  	var status = db.root.ref('/players').push('on');
+  	const status = db.root.ref('/players').push('on');
   	db.root.ref('/players').once('value', snap =>{
   		players = Object.keys(snap.val());
-  		console.log(snap.val());
-
   		me = Object.keys(snap.val()).length - 1;
   	});
   	status.onDisconnect().remove();
 
   }
 });
+//==================== Local copy of DB ==========
+
+db.players.on('value', function(snap){
+	
+	console.log('players');
+	console.log(snap.val());
+});
 //================================================
+
+
+
+
+
+
+$(document).ready(function(){
+
+	
+	// Click Events	=================
+	// Start Form
+	$$.btnStart.on("click", function(e){
+		e.preventDefault();
+		var player = $("#player-name").val().trim();
+		write.player(player, players, me);
+	});
+
+	// P1 Panel click event
+	$$.p1Chose.on("click", function(){
+		var chose = $(this).attr("data-value").trim();
+		write.playerChose(chose,players[me]);			
+	});
+
+	// P2 Panel click event
+	$$.p2Chose.on("click", function(){
+		var chose = $(this).attr("data-value").trim();
+	});
+});
+
+
+var write = {
+	player: function(name,arrPlayers,player){
+		const dbRef = firebase.database().ref('/players').child(arrPlayers[player]);
+		dbRef.set({
+		  username: name,
+		  chose :'',
+		  player : 'Player '+(player+1),
+		  score :{
+		  	win : 0,
+		  	lose : 0
+		  }
+		});
+	},
+	playerChose(myChose, key){
+		console.log(key);
+		const dbRef = firebase.database().ref('/players/'+key+'/').child('chose');
+		dbRef.set(myChose);
+	},
+
+}
 
 
 
@@ -108,55 +165,6 @@ db.p2.on('value', snap => {
 	$$.p2Win.text(obj.score.win);
 	$$.p2Lose.text(obj.score.lose);
 });*/
-
-
-$(document).ready(function(){
-
-	
-	// Click Events	=================
-	// Start Form
-	$$.btnStart.on("click", function(e){
-		e.preventDefault();
-
-		var player = $("#player-name").val().trim();
-		write(player, players, me);
-
-		console.log(player);
-	});
-	// P1 Panel click event
-	$$.p1Chose.on("click", function(){
-		var chose = $(this).attr("data-value").trim();
-		db.p1.child('chose').set(chose);		
-		
-	});
-	// P2 Panel click event
-	$$.p2Chose.on("click", function(){
-		var chose = $(this).attr("data-value").trim();
-		db.p2.child('chose').set(chose);		
-	
-	});
-});
-
-
-//-------------------------- insert player in data base
-function write(name, arrPlayers, player) {
-  const user = firebase.database().ref('/players').child(arrPlayers[player]);
-
-  user.set({
-    username: name,
-    chose :'',
-    player : 'p'+(player+1),
-    score :{
-    	win : 0,
-    	lose : 0
-    }
-  });
-}
-
-
-
-
-
 
 
 
